33b1207a1cc82ce8606ea1a06d8dc3e96a75a710
root <root@hwsrv-1033577.hostwindsdns.com>
2023-01-17 03:36:59 +0000

    init bbs.ourrea.net checkin

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'port' => 3306,
+    'database' => 'bbs.ourrea.net',
+    'username' => 'bbs.ourrea.net',
+    'password' => 'Ourrea7375@',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'bmdcxt',
+    'strict' => false,
+    'engine' => 'InnoDB',
+    'prefix_indexes' => true,
+  ),
+  'url' => 'https://bbs.ourrea.net',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
